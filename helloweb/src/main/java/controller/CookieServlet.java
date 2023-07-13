9df31df881d73eaba1f9b3ad9547949a9b1395bf
leejuno <zhqkfxm0127@naver.com>
2023-07-13 13:27:49 +0900

    Cookie 연습

---
+++ helloweb/src/main/java/controller/CookieServlet.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int visitCount = 0;
+		
+		// 쿠키읽기, 헤더에서 읽고
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null && cookies.length > 0) {
+			for(Cookie cookie: cookies) {
+				if("visitCount".equals(cookie.getName())) {
+					visitCount = Integer.parseInt(cookie.getValue());
+				}
+			}
+		}
+		// 방문횟수증가
+		visitCount++;
+		// 쿠키 쓰기, 응답헤더에서 쓰기
+		Cookie cookie = new Cookie("visitCount", String.valueOf(visitCount));
+		cookie.setPath(request.getContextPath());
+		cookie.setMaxAge(24 * 60 * 60);//1day, expire
+		response.addCookie(cookie);
+		
+		// 화면출력
+		response.setContentType("text/html; charset=utf-8");
+		response.getWriter().println("<h1>방문횟수:" + visitCount + "</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
